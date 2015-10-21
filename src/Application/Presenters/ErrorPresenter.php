966422d59379e2473767b021ce8a7ed8e839ed02
Jan Jakes <jan.jakes@tripomatic.com>
2015-10-21 13:55:28 +0200

    Improved ErrorPresenter extensibility

--- src/Application/Presenters/ErrorPresenter.php
+++ src/Application/Presenters/ErrorPresenter.php
@@ -4,72 +4,15 @@ namespace Tripomatic\NetteApi\Application\Presenters;
 
 use Nette\Application\BadRequestException;
 use Nette\Application\IPresenter;
+use Nette\Application\IResponse;
 use Nette\Application\Request;
 use Tracy\ILogger;
 use Tripomatic\NetteApi\Application\Responses\JsonResponse;
 
 class ErrorPresenter implements IPresenter
 {
-	private static $errors = [
-		100 => 'Continue',
-		101 => 'Switching Protocols',
-		102 => 'Processing',
-		200 => 'OK',
-		201 => 'Created',
-		202 => 'Accepted',
-		203 => 'Non-Authoritative Information',
-		204 => 'No Content',
-		205 => 'Reset Content',
-		206 => 'Partial Content',
-		207 => 'Multi-Status',
-		300 => 'Multiple Choices',
-		301 => 'Moved Permanently',
-		302 => 'Found',
-		303 => 'See Other',
-		304 => 'Not Modified',
-		305 => 'Use Proxy',
-		306 => 'Switch Proxy',
-		307 => 'Temporary Redirect',
-		400 => 'Bad Request',
-		401 => 'Unauthorized',
-		402 => 'Payment Required',
-		403 => 'Forbidden',
-		404 => 'Not Found',
-		405 => 'Method Not Allowed',
-		406 => 'Not Acceptable',
-		407 => 'Proxy Authentication Required',
-		408 => 'Request Timeout',
-		409 => 'Conflict',
-		410 => 'Gone',
-		411 => 'Length Required',
-		412 => 'Precondition Failed',
-		413 => 'Request Entity Too Large',
-		414 => 'Request-URI Too Long',
-		415 => 'Unsupported Media Type',
-		416 => 'Requested Range Not Satisfiable',
-		417 => 'Expectation Failed',
-		418 => 'I\'m a teapot',
-		422 => 'Unprocessable Entity',
-		423 => 'Locked',
-		424 => 'Failed Dependency',
-		425 => 'Unordered Collection',
-		426 => 'Upgrade Required',
-		449 => 'Retry With',
-		450 => 'Blocked by Windows Parental Controls',
-		500 => 'Internal Server Error',
-		501 => 'Not Implemented',
-		502 => 'Bad Gateway',
-		503 => 'Service Unavailable',
-		504 => 'Gateway Timeout',
-		505 => 'HTTP Version Not Supported',
-		506 => 'Variant Also Negotiates',
-		507 => 'Insufficient Storage',
-		509 => 'Bandwidth Limit Exceeded',
-		510 => 'Not Extended'
-	];
-
 	/** @var ILogger|NULL */
-	private $logger;
+	protected $logger;
 
 	/**
 	 * @param ILogger $logger
@@ -99,12 +42,22 @@ class ErrorPresenter implements IPresenter
 			}
 		}
 
-		if (isset(self::$errors[$code])) {
-			$message = self::$errors[$code];
+		if (isset(JsonResponse::$messages[$code])) {
+			$message = JsonResponse::$messages[$code];
 		} else {
 			$message = 'Unknown error';
 		}
 
+		return $this->createResponse($message, $code);
+	}
+
+	/**
+	 * @param string $message
+	 * @param int $code
+	 * @return IResponse
+	 */
+	protected function createResponse($message, $code)
+	{
 		return new JsonResponse(['message' => $message], $code);
 	}
 }
